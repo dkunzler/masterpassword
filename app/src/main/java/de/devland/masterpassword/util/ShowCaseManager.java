46656fe696c90dbaf2a050530498e2394fcf10b4
David Kunzler <dk@devland.de>
2014-10-01 22:50:13 +0200

    catch exceptions from showcaseview to prevent crash every start

--- app/src/main/java/de/devland/masterpassword/util/ShowCaseManager.java
+++ app/src/main/java/de/devland/masterpassword/util/ShowCaseManager.java
@@ -29,56 +29,69 @@ public enum ShowCaseManager {
 
     public void showEditShowCase(Activity activity, View target) {
         if (!showCasePrefs.editShown()) {
-            ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
-            showCaseBuilder.hideOnTouchOutside()
-                    .setContentTitle(R.string.title_edit)
-                    .setStyle(R.style.ShowcaseLightTheme)
-                    .setContentText(R.string.content_edit)
-                    .setTarget(new ViewTarget(target));
-            showCaseBuilder.build().show();
-            showCasePrefs.editShown(true);
+            try {
+                ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
+                showCaseBuilder.hideOnTouchOutside().setContentTitle(R.string.title_edit)
+                               .setStyle(R.style.ShowcaseLightTheme)
+                               .setContentText(R.string.content_edit)
+                               .setTarget(new ViewTarget(target));
+                showCaseBuilder.build().show();
+            } catch (IllegalArgumentException e) {
+            } finally {
+                showCasePrefs.editShown(true);
+            }
         }
     }
 
     public void showLoginShowCase(Activity activity, View target) {
         if (!showCasePrefs.loginShown()) {
-            ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
-            showCaseBuilder.hideOnTouchOutside()
-                    .setContentTitle(R.string.title_login)
-                    .setStyle(R.style.ShowcaseLightTheme)
-                    .setContentText(R.string.content_login)
-                    .setTarget(new ViewTarget(target));
-            showCaseBuilder.build().show();
-            showCasePrefs.loginShown(true);
+            try {
+                ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
+                showCaseBuilder.hideOnTouchOutside().setContentTitle(R.string.title_login)
+                               .setStyle(R.style.ShowcaseLightTheme)
+                               .setContentText(R.string.content_login)
+                               .setTarget(new ViewTarget(target));
+                showCaseBuilder.build().show();
+            } catch (IllegalArgumentException e) {
+            } finally {
+                showCasePrefs.loginShown(true);
+            }
         }
     }
 
     public void showAddShowCase(Activity activity) {
         if (!showCasePrefs.addCardShown()) {
-            ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
-            showCaseBuilder.hideOnTouchOutside()
-                    .setContentTitle(R.string.title_add)
-                    .setStyle(R.style.ShowcaseLightTheme)
-                    .setContentText(R.string.content_add)
-                    .setTarget(new ActionItemTarget(activity, R.id.action_add));
-            showCaseBuilder.build().show();
-            showCasePrefs.addCardShown(true);
+            try {
+
+                ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
+                showCaseBuilder.hideOnTouchOutside().setContentTitle(R.string.title_add)
+                               .setStyle(R.style.ShowcaseLightTheme)
+                               .setContentText(R.string.content_add)
+                               .setTarget(new ActionItemTarget(activity, R.id.action_add));
+                showCaseBuilder.build().show();
+            } catch (IllegalArgumentException e) {
+            } finally {
+                showCasePrefs.addCardShown(true);
+            }
         }
     }
 
     public void showFirstCardShowCase(Activity activity) {
         if (!showCasePrefs.firstCardShown()) {
-            ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
-            Display display = activity.getWindowManager().getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
+            try {
+                ShowcaseView.Builder showCaseBuilder = new ShowcaseView.Builder(activity, true);
+                Display display = activity.getWindowManager().getDefaultDisplay();
+                Point size = new Point();
+                display.getSize(size);
 
-            showCaseBuilder.hideOnTouchOutside()
-                    .setStyle(R.style.ShowcaseLightTheme)
-                    .setContentText(R.string.content_firstCard)
-                    .setTarget(new PointTarget(size.x / 2, size.y / 4));
-            showCaseBuilder.build().show();
-            showCasePrefs.firstCardShown(true);
+                showCaseBuilder.hideOnTouchOutside().setStyle(R.style.ShowcaseLightTheme)
+                               .setContentText(R.string.content_firstCard)
+                               .setTarget(new PointTarget(size.x / 2, size.y / 4));
+                showCaseBuilder.build().show();
+            } catch (IllegalArgumentException e) {
+            } finally {
+                showCasePrefs.firstCardShown(true);
+            }
         }
     }
 }
