9fbdf4f4eb3090013e9af868555fa0f47c6db56b
dk <dk@davidkunzler.de>
2014-10-26 22:46:06 +0100

    aFileChooser with file extension filter, re-added SAF for import on KitKat and beyond

--- app/src/main/java/de/devland/masterpassword/export/Importer.java
+++ app/src/main/java/de/devland/masterpassword/export/Importer.java
@@ -1,12 +1,15 @@
 package de.devland.masterpassword.export;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.ipaulpro.afilechooser.FileChooserActivity;
 import com.ipaulpro.afilechooser.utils.FileUtils;
 
 import java.io.BufferedReader;
@@ -15,6 +18,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import de.devland.masterpassword.R;
@@ -35,15 +39,40 @@ public class Importer implements RequestCodeManager.RequestCodeCallback {
     public void startImportIntent(Activity activity, ImportType importType) {
         this.activity = activity;
 
-        Intent getContentIntent = FileUtils.createGetContentIntent();
-        getContentIntent.setType("text/plain");
+        Intent intent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            intent = getStorageAccessFrameworkIntent();
+        } else {
+            intent = getLegacyFileChooserIntent();
+        }
 
-        Intent intent = Intent.createChooser(getContentIntent, activity.getString(R.string.caption_selectFile));
         Bundle extraData = new Bundle();
         extraData.putSerializable(EXTRA_IMPORT_TYPE, importType);
         int requestCode = RequestCodeManager.INSTANCE
                 .addRequest(REQUEST_CODE_IMPORT, this.getClass(), this, extraData);
         activity.startActivityForResult(intent, requestCode);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private Intent getStorageAccessFrameworkIntent() {
+        Intent intent;
+        intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as
+        // a file (as opposed to a list of contacts or timezones).
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Create a file with the requested MIME type.
+        intent.setType("text/plain");
+        return intent;
+    }
+
+    private Intent getLegacyFileChooserIntent() {
+        Intent getContentIntent = FileUtils.createGetContentIntent();
+        getContentIntent.putStringArrayListExtra(FileChooserActivity.EXTRA_FILTER_INCLUDE_EXTENSIONS, new ArrayList<>(Arrays.asList(".json", ".mpsites")));
+        getContentIntent.setType("text/plain");
+        Intent intent = Intent.createChooser(getContentIntent, activity.getString(R.string.caption_selectFile));
+        return intent;
     }
 
     @Override
