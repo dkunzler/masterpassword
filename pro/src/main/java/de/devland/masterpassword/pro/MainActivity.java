3323d86fcaf8dea6ea73419ed4feb0f0689ecc12
dk <dk@davidkunzler.de>
2014-12-30 23:37:09 +0100

    "Thank You" message in Pro App

--- pro/src/main/java/de/devland/masterpassword/pro/MainActivity.java
+++ pro/src/main/java/de/devland/masterpassword/pro/MainActivity.java
@@ -1,17 +1,26 @@
 package de.devland.masterpassword.pro;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import de.devland.masterpassword.shared.ui.BaseActivity;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        Intent service = new Intent(this, LicenseCheckService.class);
+        startService(service);
     }
 
 
